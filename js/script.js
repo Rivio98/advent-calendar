208e3e235ef59248267d8c7fea61d4c1eaa41ab6
Rivio98 <rivaldogjoni1998@gmail.com>
2024-12-16 18:25:31 +0100

    [UPD]First try

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 import source from './store.js';
+
+const calendar = document.getElementById('calendar');
+
+// crea caselle per il calendario
+
+for (let i = 1; i <= 25; i++) {
+    const column = document.createElement('div');
+    column.className = (i === 25) ? 'col-12' : 'col-2';
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.textContent = i;
+
+    column.appendChild(box);
+    calendar.appendChild(column);
+}
